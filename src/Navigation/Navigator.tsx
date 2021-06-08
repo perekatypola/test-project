ed72f7d7c32ceb3bfce201bb365ff20e749639cc
perekatypola <pola.royn@tut.by>
2021-06-08 12:12:46 +0300

    fix eslint work

--- src/Navigation/Navigator.tsx
+++ src/Navigation/Navigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type {FC} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../HomeScreen/HomeScreen';
 
 const Stack = createStackNavigator();
