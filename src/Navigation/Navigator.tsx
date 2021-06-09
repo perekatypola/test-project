d05aaaac8129cb24e7156b59659e06e8acf006ad
perekatypola <pola.royn@tut.by>
2021-06-09 11:13:52 +0300

    fix problems in config

--- src/Navigation/Navigator.tsx
+++ src/Navigation/Navigator.tsx
@@ -1,20 +1,19 @@
+import React from 'react';
+import type {FC} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../HomeScreen';
+import CharacterScreen from '../Screens/CharacterScreen';
 
-import React from "react";
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, StackView } from '@react-navigation/stack';
-const Stack = createStackNavigator();
-import HomeScreen from "../HomeScreen/HomeScreen";
-import type { FC } from "react";
+const Tab = createBottomTabNavigator();
 
-const Navigator:FC =() => {
-        return (
-            <NavigationContainer>
-              <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-              </Stack.Navigator>
-            </NavigationContainer>
-          ); 
-}
+const Navigator: FC = () => (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Characters" component={CharacterScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
 
 export default Navigator;
