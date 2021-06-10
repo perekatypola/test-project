a0515676e9afb590b6176077b784c98f90dcba45
perekatypola <pola.royn@tut.by>
2021-06-10 23:24:04 +0300

    fix home screen

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import {View} from 'react-native';
 import styles from './styles';
 
-const Home: React.FC = () => <View style={styles.view} />;
+const Home: React.FC = () => <View style={styles.container} />;
 export default Home;
