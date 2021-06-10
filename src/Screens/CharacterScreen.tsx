872cf41f387e3af257815527acbcb037501e87cd
perekatypola <pola.royn@tut.by>
2021-06-10 11:55:51 +0300

    add error proccessing

--- src/Screens/CharacterScreen.tsx
+++ src/Screens/CharacterScreen.tsx
@@ -3,20 +3,26 @@ import {View, Text, FlatList} from 'react-native';
 import {useSelector} from 'react-redux';
 import RootState, {store} from '../redux/store';
 import actions from '../redux/actions/actions';
+import ErrorBoundary from '../ErrorProccessing/ErrorBoundary';
+
+const selectCharacters = (state: RootState) => state.characters;
 
 const CharacterScreen = () => {
-  const selectCharacters = (state: RootState) => state.characters;
   const {characters} = useSelector(selectCharacters);
+
   useEffect(() => {
     store.dispatch(actions.addCharactersThunk());
   }, []);
+
   return (
     <View>
-      <FlatList
-        data={characters}
-        renderItem={({item}) => <Text>{item.name}</Text>}
-        keyExtractor={item => item.name}
-      />
+      <ErrorBoundary>
+        <FlatList
+          data={characters}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+          keyExtractor={item => item.name}
+        />
+      </ErrorBoundary>
     </View>
   );
 };
