42a8f8e517f57f3f1ebb99f277310939eb8d3cfd
perekatypola <pola.royn@tut.by>
2021-06-14 20:51:36 +0300

    implement i18next

--- src/containers/Screens/Home/index.tsx
+++ src/containers/Screens/Home/index.tsx
@@ -1,19 +1,30 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, Text} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import i18n from '../../../Localization';
 import styles from './styles';
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
+  const {t} = useTranslation();
 
   return (
     <View style={styles.container}>
       <Button
-        title="Discover Rick and Morty"
+        title="rus"
+        onPress={() => {
+          i18n.changeLanguage('ru');
+        }}>
+        RU
+      </Button>
+      <Button
+        title="Rick and Morty"
         onPress={() => {
           navigation.navigate('Rick and Morty');
         }}
       />
+      <Text>{t('description.homeName')}</Text>
     </View>
   );
 };
