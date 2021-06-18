2ca6462c65b90ac927b15fa67d5d41b32aad661f
perekatypola <61651044+perekatypola@users.noreply.github.com>
2021-06-18 13:39:36 +0300

    Delete src/Store directory

--- src/Store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import charactersReducer from './characters';
-
-const rootReducer = combineReducers({
-  characters: charactersReducer,
-});
-
-export default rootReducer;
